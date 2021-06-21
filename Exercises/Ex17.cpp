a3ef21bb4f4259440ca3d09910c350e08bc02709
Srivatsan T <CS20BTECH11062@iith.ac.in>
2021-06-21 15:02:14 +0530

    Ex17

--- Exercises/Ex17.cpp
+++ Exercises/Ex17.cpp
@@ -5,41 +5,79 @@ template <typename T>
 class sort_t
 {
 public:
-    std::vector<T> list;
     virtual std::vector<T> sort_list() = 0;
+    void print_vec(std::vector<T> a)
+    {
+        for (unsigned i = 0; i < a.size(); i++)
+        {
+            std::cout << a[i] << ":";
+        }
+        std::cout << std::endl;
+    }
 };
 
 template <typename T>
 class quick_sort_t : public sort_t<T>
 {
+    std::vector<T> list;
+
 public:
+    quick_sort_t(std::vector<T> i)
+    {
+        list = i;
+    }
+    void swap(T *a, T *b)
+    {
+        T t = *a;
+        *a = *b;
+        *b = t;
+    }
+
+    /* This function takes last element as pivot, places
+the pivot element at its correct position in sorted
+array, and places all smaller (smaller than pivot)
+to left of pivot and all greater elements to right
+of pivot */
     int partition(std::vector<T> arr, int low, int high)
     {
-        int pivot = arr[high];
-        int i = (low - 1);
+        T pivot = arr[high]; // pivot
+        int i = (low - 1);     // Index of smaller element and indicates the right position of pivot found so far
 
         for (int j = low; j <= high - 1; j++)
         {
+            // If current element is smaller than the pivot
             if (arr[j] < pivot)
             {
-                i++;
-                std::swap(&arr[i], &arr[j]);
+                i++; // increment index of smaller element
+                swap(&arr[i], &arr[j]);
             }
         }
-        std::swap(&arr[i + 1], &arr[high]);
+        swap(&arr[i + 1], &arr[high]);
         return (i + 1);
+    }
+
+    /* The main function that implements QuickSort
+arr[] --> Array to be sorted,
+low --> Starting index,
+high --> Ending index */
+    void quickSort(std::vector<T> arr, int low, int high)
+    {
+        if (low < high)
+        {
+            /* pi is partitioning index, arr[p] is now
+        at right place */
+            int pi = partition(arr, low, high);
+
+            // Separately sort elements before
+            // partition and after partition
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
     }
 
     std::vector<T> sort_list()
     {
-        int low = 0;
-        int high = list.size() - 1;
-        if (low < high)
-        {
-            int pi = partition(list, low, high);
-            sort_list(list, low, pi - 1);
-            sort_list(list, pi + 1, high);
-        }
+        quickSort(list,0,list.size()-1);
         return list;
     }
 };
@@ -47,87 +85,98 @@ public:
 template <typename T>
 class merge_sort_t : public sort_t<T>
 {
+    std::vector<T> list;
+
 public:
+    merge_sort_t(std::vector<T> i)
+    {
+        list = i;
+    }
     void merge(std::vector<T> arr, int l, int m, int r)
     {
         int n1 = m - l + 1;
-    int n2 = r - m;
+        int n2 = r - m;
 
-    // Create temp arrays
-    int L[n1], R[n2];
+        // Create temp arrays
+        int L[n1], R[n2];
 
-    // Copy data to temp arrays L[] and R[]
-    for (int i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-    for (int j = 0; j < n2; j++)
-        R[j] = arr[m + 1 + j];
+        // Copy data to temp arrays L[] and R[]
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[m + 1 + j];
 
-    // Merge the temp arrays back into arr[l..r]
+        // Merge the temp arrays back into arr[l..r]
 
-    // Initial index of first subarray
-    int i = 0;
+        // Initial index of first subarray
+        int i = 0;
 
-    // Initial index of second subarray
-    int j = 0;
+        // Initial index of second subarray
+        int j = 0;
 
-    // Initial index of merged subarray
-    int k = l;
+        // Initial index of merged subarray
+        int k = l;
 
-    while (i < n1 && j < n2)
-    {
-        if (L[i] <= R[j])
+        while (i < n1 && j < n2)
+        {
+            if (L[i] <= R[j])
+            {
+                arr[k] = L[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1)
         {
             arr[k] = L[i];
             i++;
+            k++;
         }
-        else
+        while (j < n2)
         {
             arr[k] = R[j];
             j++;
+            k++;
         }
-        k++;
     }
-
-    while (i < n1)
+    void mergeSort(std::vector<T> arr, int l, int r)
     {
-        arr[k] = L[i];
-        i++;
-        k++;
+        if (l >= r)
+        {
+            return; //returns recursively
+        }
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
     }
-    while (j < n2)
+    std::vector<T> sort_list()
     {
-        arr[k] = R[j];
-        j++;
-        k++;
+        mergeSort(list, 0, list.size() - 1);
+        return list;
     }
-}
-
-
-std::vector<T> sort_list()
-{
-    int l = 0;
-    int r = list.size() - 1;
-    if (l >= r)
-    {
-        return; //returns recursively
-    }
-    int m = l + (r - l) / 2;
-    mergeSort(list, l, m);
-    mergeSort(list, m + 1, r);
-    merge(list, l, m, r);
-    return list;
-}
-
 };
 
 template <typename T>
 class insertion_sort_t : public sort_t<T>
 {
+    std::vector<T> list;
+
 public:
+    insertion_sort_t(std::vector<T> i)
+    {
+        list = i;
+    }
     std::vector<T> sort_list()
     {
         int n = list.size();
-        int i,j;
+        int i, j;
         T key;
         for (i = 1; i < n; i++)
         {
@@ -146,4 +195,16 @@ public:
         }
         return list;
     }
+};
+
+int main()
+{
+    std::vector<int> a = {2, 13, 5, 34, 1, 43};
+    sort_t<int> *t1 = new insertion_sort_t<int>(a);
+    sort_t<int> *t2 = new merge_sort_t<int>(a);
+    sort_t<int> *t3 = new quick_sort_t<int>(a);
+    t1->print_vec(t1->sort_list());
+    t2->print_vec(t2->sort_list());
+    t3->print_vec(t3->sort_list());
+    return 0;
 }
